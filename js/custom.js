624d4b874d43f22cd84ceac5fe950c2f00b3682e
DenisS <den_sorokin_1@mail.ru>
2019-03-28 18:33:18 +0300

    1/2 ready

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,27 @@
-var uid2guid = function(uid) {
- if (!uid) {
-   return;
- }
-
- var steamId = bigInt(uid);
-
- var parts = [0x42,0x45,0,0,0,0,0,0,0,0];
-
- for (var i = 2; i < 10; i++) {
-   var res = steamId.divmod(256);
-   steamId = res.quotient; 
-   parts[i] = res.remainder.toJSNumber();
- }
-
- var wordArray = CryptoJS.lib.WordArray.create(new Uint8Array(parts));
- var hash = CryptoJS.MD5(wordArray);
- return hash.toString();
-};
-
+$( "#Myconvert" ).click(function() {
+  let uid2guid = function(uid) {
+    if (!uid) {
+      return;
+    }
+   
+    let steamId = bigInt(uid);
+   
+    let parts = [0x42,0x45,0,0,0,0,0,0,0,0];
+   
+    for (let i = 2; i < 10; i++) {
+      let res = steamId.divmod(256);
+      steamId = res.quotient; 
+      parts[i] = res.remainder.toJSNumber();
+    }
+   
+    let wordArray = CryptoJS.lib.WordArray.create(new Uint8Array(parts));
+    let hash = CryptoJS.MD5(wordArray);
+    return hash.toString();
+   };
+   
+   
+   
+});
 
 
 
